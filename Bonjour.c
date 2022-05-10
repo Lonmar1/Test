fd02bb8d92a9e5574890afc602d2ad5f1f1e9d5b
Lonmar1 <88637049+Lonmar1@users.noreply.github.com>
2022-05-10 21:27:06 +0200

    Add files via upload

---
+++ Bonjour.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main () {
+	puts ("Bonjour, appuie sur une touche s'il te plait");
+	getchar ();
+	puts ("Encore une fois pour etre sûr...");
+	getchar ();
+	puts ("Merci d'avoir appuyé sur une touche :)");
+	return 0;
+} 
